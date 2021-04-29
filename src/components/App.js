c9bd5454395e041b325d014b305ea7384096e978
AbdulAahad13 <76912038+AbdulAahad13@users.noreply.github.com>
2021-04-30 01:35:28 +0600

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 import unsplash from '../api/unsplash'
-
+import imageList from './imageList'
 class App extends React.Component {
     state = {
         images: []
@@ -21,7 +21,7 @@ class App extends React.Component {
         return (
             <div className='ui container' style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
-                Found: {this.state.images.length} images
+                <imageList images={this.state.images}/>
             </div>)
 
 
